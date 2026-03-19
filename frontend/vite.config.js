6ed222aa87426bbda8b38cfc59917219e0065771
pleezgo <zaharsolar8@gmail.com>
2026-03-19 20:35:56 +0200

    fix: add vite preview host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    allowedHosts: process.env.ALLOWED_HOSTS
+      ? process.env.ALLOWED_HOSTS.split(',')
+      : [],
+  },
 })
